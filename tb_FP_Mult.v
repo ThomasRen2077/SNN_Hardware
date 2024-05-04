fce95b605ce1f04d951e6d5b25e9fd9f1fa66b6e
ThomasRen2077 <zr86@cornell.edu>
2024-05-03 22:06:07 -0400

    update testbench of Floating Point Multiplier

--- tb_FP_Mult.v
+++ tb_FP_Mult.v
@@ -26,12 +26,20 @@ always #((CLOCK_PERIOD / 2)) clk = ~clk;
 // Test stimulus
 initial begin
     // Initialize inputs
-    a_input = 32'h40400000; 	// 3.0 in IEEE 754 format
-    b_input = 32'h40A00000; 	// 5.0 in IEEE 754 format
-    
-  #(10 * CLOCK_PERIOD);
     a_input = 32'h4048f5c3; 	// 3.14 in IEEE 754 format
     b_input = 32'h40A00000; 	// 5.0 in IEEE 754 format
+  
+    #50;
+    a_input = 32'h4048f5c3; 	// 3.14 in IEEE 754 format
+    b_input = 32'hc0a00000; 	// -5.0 in IEEE 754 format
+    #50;
+    a_input = 32'hc048f5c3; 	// -3.14 in IEEE 754 format
+    b_input = 32'h40A00000; 	// 5.0 in IEEE 754 format
+  
+    #50;
+    a_input = 32'hc048f5c3; 	// -3.14 in IEEE 754 format
+    b_input = 32'hc0a00000; 	// -5.0 in IEEE 754 format
+
     #200;    
     $finish;
 end
